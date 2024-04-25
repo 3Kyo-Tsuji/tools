6b9afa27d9f2f3cac83956b7301d2f7b4c66c74e
E.Tsuji <tsuji.e@3kyo.jp>
2024-04-25 17:23:58 +0900

    サジェストリスト 追加

--- サジェストリスト/search.php
+++ サジェストリスト/search.php
@@ -1,10 +1,10 @@
 <?php
 if ($_POST["q"]) {
-    $user = 'DB_USER';
-    $password = 'DB_PASSWORD';
-    $db = 'DB_NAME';
-    $host = 'DB_HOST';
-    $pdo = new PDO("mysql:host={$host};dbname={$db};charset=utf8", $user, $password);
+    // $user = 'DB_USER';
+    // $password = 'DB_PASSWORD';
+    // $db = 'DB_NAME';
+    // $host = 'DB_HOST';
+    // $pdo = new PDO("mysql:host={$host};dbname={$db};charset=utf8", $user, $password);
 
     try {
         $query = $_POST["q"];
@@ -12,11 +12,15 @@ if ($_POST["q"]) {
         if ($query == "") {
             echo json_encode([]);
         } else {
-            // タグを入力内容でLIKE検索(前方一致)
-            $sql = "SELECT tag FROM jbr_program WHERE tag LIKE '%{$query}%'";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_COLUMN);
+            // // タグを入力内容でLIKE検索(前方一致)
+            // $sql = "SELECT tag FROM jbr_program WHERE tag LIKE '%{$query}%'";
+            // $stmt = $pdo->prepare($sql);
+            // $stmt->execute();
+            // $result = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+            $array = ['apple', 'banana', 'orange', 'melon'];
+
+            $result = preg_grep("/{$_POST['q']}/", $array);
 
             // jsonに変換して返す
             echo json_encode(array_unique($result));
--- サジェストリスト/sugest-input.js
+++ サジェストリスト/sugest-input.js
@@ -2,61 +2,89 @@ function suggestGenerate(input) {
     // サジェスト用のロストを作成
     document.getElementById(input).insertAdjacentHTML('afterend', '<ul id="suggest-list-' + input + '" class="suggest-list"></ul>');
 
-    // テキスト入力があった際の挙動 ※JSの場合
+    // テキスト入力があった際の挙動
     document.getElementById(input).addEventListener("input", (e) => {
-        const word = e.target.value;
-        const list = [
-            'apple',
-            'banana',
-            'orange',
-            'melon'
-        ];
-
-        const suggestBox = document.getElementById('suggest-list-' + input);
-        suggestBox.innerHTML = '';
-        for (const suggest of list) {
-            if (word !== '' && 0 <= suggest.indexOf(word)) {
-                let li = document.createElement('li');
-                // サジェストされたアイテムをクリックした際の挙動
-                li.onclick = function () {
-                    document.getElementById(input).value = suggest;
-                    suggestBox.innerHTML = '';
+        // 動的にフォームを作成し非同期のPHPでデータを取得
+        const fd = new FormData();
+        fd.append('q', e.target.value);
+        const param = {
+            method: "POST",
+            body: fd
+        }
+        // 非同期でタグを取得
+        fetch("search.php", param)
+            .then((response) => response.json())
+            .then((json) => {
+                const suggestBox = document.getElementById('suggest-list-' + input);
+                suggestBox.innerHTML = '';
+                // 取得した内容をリストに追加
+                for (const suggest of json) {
+                    let li = document.createElement('li');
+                    // サジェストされたアイテムをクリックした際の挙動
+                    li.onclick = function () {
+                        document.getElementById(input).value = suggest;
+                        suggestBox.innerHTML = '';
+                    }
+                    let txt = document.createTextNode(suggest);
+                    li.appendChild(txt);
+                    suggestBox.appendChild(li);
                 }
-                let txt = document.createTextNode(suggest);
-                li.appendChild(txt);
-                suggestBox.appendChild(li);
+            });
+    });
+
+    // タグ設定
+    document.getElementById(input).addEventListener("keypress", (e) => {
+        const id = '#suggest-list-' + input;
+        // Enterキーで発火
+        if (e.key === "Enter") {
+            // selectされているリストの文字列をインプットにセット
+            e.target.value = document.querySelector(id + ' .selected').textContent;
+            // サジェストリストをクリア
+            document.querySelector(id).innerHTML = '';
+        }
+    });
+
+    // 方向キーが押された際の挙動
+    document.getElementById(input).addEventListener("keyup", (e) => {
+        const id = '#suggest-list-' + input;
+        // 下キー
+        if (e.key === "ArrowDown") {
+            // 未選択の場合、サジェストリストにカーソル
+            if (document.querySelector(id + ' .selected') === null) {
+                if (document.querySelector(id + ' li') !== null) {
+                    document.querySelector(id + ' li').classList.add('selected');
+                }
+            } else {
+                const list = document.querySelectorAll(id + ' li');
+                let selectFlg = false;
+                for (let i = 0; i < list.length; i++) {
+                    // selectedクラスがあればremove
+                    if (list[i].classList.contains('selected')) {
+                        if (i !== list.length - 1) {
+                            list[i].classList.remove('selected');
+                            selectFlg = true;
+                        }
+                    } else {
+                        if (selectFlg) {
+                            list[i].classList.add('selected');
+                            selectFlg = false;
+                        }
+                    }
+                }
+            }
+        // 上キー
+        } else if (e.key === "ArrowUp") {
+            const list = document.querySelectorAll(id + ' li');
+            let selectFlg = false;
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].classList.contains('selected') && i !== 0) {
+                    list[i].classList.remove('selected');
+                    list[i - 1].classList.add('selected');
+                }
             }
         }
     });
 
-    // // テキスト入力があった際の挙動 ※PHPも使用する場合
-    // document.getElementById(input).addEventListener("input", (e) => {
-    //     // 動的にフォームを作成し非同期のPHPでデータを取得
-    //     const fd = new FormData();
-    //     fd.append('q', e.target.value);
-    //     const param = {
-    //         method: "POST",
-    //         body: fd
-    //     }
-    //     // 非同期でタグを取得
-    //     fetch("tagSearch.php", param)
-    //         .then((response) => response.json())
-    //         .then((json) => {
-    //             const suggestBox = document.getElementById('suggest-list-' + input);
-    //             suggestBox.innerHTML = '';
-    //             // 取得した内容をリストに追加
-    //             for (const suggest of json) {
-    //                 let li = document.createElement('li');
-    //                 // サジェストされたアイテムをクリックした際の挙動
-    //                 li.onclick = function () {
-    //                     document.getElementById(input).value = suggest;
-    //                     suggestBox.innerHTML = '';
-    //                 }
-    //                 let txt = document.createTextNode(suggest);
-    //                 li.appendChild(txt);
-    //                 suggestBox.appendChild(li);
-    //             }
-    //         });
-    // });
+
 }
 
